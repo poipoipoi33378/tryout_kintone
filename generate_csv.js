c82768986ac790c897f88a6c1d2b5c3722c8b208
Seiichi Sugahara <poipoipoi33378@gmail.com>
2018-12-05 21:34:28 +0900

    課題２：CSV出力ボタンを設置する

--- generate_csv.js
+++ generate_csv.js
@@ -1,4 +1,23 @@
 (function () {
     "use strict";
-    window.alert('はじめようkintone JavaScript API');
+
+    kintone.events.on('app.record.index.show', function (event) {
+        if (event.viewName == "出力用一覧（当月&出力済除外）"){
+            if (document.getElementById('my_index_button') !== null) {
+                return;
+            }
+            // ボタン
+            var myIndexButton = document.createElement('button');
+            myIndexButton.id = 'my_index_button';
+            myIndexButton.innerHTML = 'CSV出力';
+
+            // ボタンクリック時の処理
+            myIndexButton.onclick = function () {
+                window.confirm('CSVを出力します');
+            };
+            // メニューの右側の空白部分にボタンを設置
+            kintone.app.getHeaderMenuSpaceElement().appendChild(myIndexButton);
+        }
+        console.log (event)
+    });
 })();
